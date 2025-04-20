47f27b419b57a7e10a9581d7cfafec491070ee7b
juliaschonfeldova <julia.schonfeldova@gmail.com>
2025-04-20 21:55:42 +0300

    Update hw2_q1.py

--- hw2_q1.py
+++ hw2_q1.py
@@ -37,3 +37,24 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+
+    with open(input_file, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    words = []
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            words += [""]
+        else:
+            words += line.upper().split()
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        for i, word in enumerate(words):
+            translated = word.translate(str.maketrans(MORSE_CODE))
+            f.write(translated)
+            # expected has no empty line in the end
+            if i != len(words) - 1:
+                f.write("\n")
+
+english_to_morse()
